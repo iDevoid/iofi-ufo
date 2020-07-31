65ed7efa3234b3a9ea216c4ed669faeae34b2354
Shodiq Muhammad <muhammadshodiq165@live.com>
2020-07-31 20:32:23 +0700

    :paw_prints: new contributors card on about

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,17 +2,35 @@
   <div class="about">
     <v-main>
       <AboutCard />
+      <ContributorsCard v-bind:contributors="contributors" />
     </v-main>
   </div>
 </template>
 
 <script>
 import AboutCard from '../components/cards/AboutCard';
+import ContributorsCard from '../components/cards/ContributorsCard';
 
 export default {
   name : 'About',
   components : {
     AboutCard,
+    ContributorsCard,
+  },
+  computed: {
+    contributors() {
+      var vc = this.$root.$t("voice-clip");
+      return [
+        {
+          name: "Raktea",
+          desc: "K-word " + vc
+        },
+        {
+          name: "Toastie",
+          desc: "Goodnight muah " + vc
+        }
+      ]
+    }
   }
 }
 </script>
