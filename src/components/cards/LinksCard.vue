31221b514ad8ac1f24b7ba9f78debfb23fa93dd2
Shodiq Muhammad <muhammadshodiq165@live.com>
2020-08-01 10:00:27 +0700

    :zap: add more related character links

--- src/components/cards/LinksCard.vue
+++ src/components/cards/LinksCard.vue
@@ -1,20 +1,23 @@
 <template>
     <div>
-        <v-col cols="12" class="float-left" v-if="links">
-            <v-card class="pa-md-2 mx-lg-auto">
+        <v-col cols="12" v-if="links">
+            <v-card class="pa-md-2">
                 <v-card-title class="headline" v-text="title"></v-card-title>
                 <v-card-actions class="d-block">
-                    <v-btn 
-                        v-for="(link, i) in links" 
-                        :key="i"  
-                        class="voice-button subtitle-1" 
-                        :color="link.color" 
-                        dark 
-                        :href="link.url"
-                        target="_blank"
-                    >
-                        {{ link.title }}
-                    </v-btn>
+                    <div 
+                        v-for="link in links" 
+                        :key="link.name"   
+                    class="pa-md-2 mx-lg-auto d-inline">
+                        <v-btn 
+                            class="v-buttons subtitle-1" 
+                            :color="link.color" 
+                            dark 
+                            :href="link.url"
+                            target="_blank"
+                            v-text="link.title"
+                        >
+                        </v-btn>
+                    </div>
                 </v-card-actions>
             </v-card>
         </v-col>
